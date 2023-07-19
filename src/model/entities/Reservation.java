0135add34bd01ade9cf35f4b2656e0ec6cf1cb00
iurypedrosa <iuryppedrosa@protonmail.com>
2023-07-18 21:24:50 -0300

    segundo commit

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -10,12 +10,15 @@ import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.util.Date;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class Reservation {
 
     private Integer roomNumber;
     private Date checkIn;
     private Date checkOut;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
         this.roomNumber = roomNumber;
@@ -52,11 +55,26 @@ public class Reservation {
     }
 
 
-    public int duration(){
-        return (int) ChronoUnit.DAYS.between((Temporal) checkIn, (Temporal) checkOut);
+    public long duration(){
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
     public void updateDates(Date checkIn, Date checkOut){
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
 
+    }
+
+    @Override
+    public String toString(){
+        return "Run"
+                + roomNumber
+                + ", check in: "
+                + sdf.format(checkIn)
+                + ", chack out: "
+                + ", "
+                + duration()
+                + "nights.";
     }
 }
